1587802275d989a96f6c2df6ab5c56a5e65f2796
jasonbrackman <brackman@gmail.com>
2019-01-13 13:25:43 -0800

    publishing route for a path -- haven't proven its the shortest.

--- src/day_15.rs
+++ src/day_15.rs
@@ -25,14 +25,14 @@ impl Combat {
 
 struct Board {
     cells: Vec<Vec<char>>,
-    paths: HashMap<(usize, usize), Vec<(usize, usize)>>,
+    cell_neighbours: HashMap<(usize, usize), Vec<(usize, usize)>>,
 }
 
 impl Board {
     fn new(input: &str) -> Board {
         let cells = Board::populate_cells(input);
-        let paths = Board::calculate_paths(&cells);
-        Board{cells, paths}
+        let cell_neighbours = Board::calculate_paths(&cells);
+        Board{cells, cell_neighbours }
     }
 
     fn populate_cells(input: &str) -> Vec<Vec<char>> {
@@ -76,29 +76,53 @@ impl Board {
     }
 
     fn calculate_shortest_path(&mut self, from: (usize, usize), to: (usize, usize)) {
+        // First attempt -- only showed me if the path was possible -- but not keeping
+        // track of the actual shorest path.
+
         let mut visited = Vec::new();
-        let mut to_visit = VecDeque::new();
+        let mut frontier = VecDeque::new();
+        frontier.insert(0, from);
 
-        to_visit.extend(self.paths.get(&from).unwrap().to_vec());
+        while !frontier.is_empty() {
+            let current = frontier.pop_front().unwrap();
 
-        while !to_visit.is_empty() {
-            let neighbour = to_visit.pop_front().unwrap();
-
-            if neighbour == to {
+            if current == to {
                 println!("Found A Path to Destination! => {:?}", visited);
                 break
             }
             else {
-                if !visited.contains(&neighbour) {
+                if !visited.contains(&current) {
                     println!("Checking neighbours... ");
-                    to_visit.extend(self.paths.get(&neighbour).unwrap().to_vec());
+                    frontier.extend(self.cell_neighbours.get(&current).unwrap().to_vec());
                 }
             }
 
-            visited.push(neighbour);
+            visited.push(current);
         }
 
         println!("Unable to find a path to destination.");
+    }
+
+    fn calculate_shortest_path2(&mut self, from: (usize, usize), to: (usize, usize)) -> HashMap<(usize, usize), (usize, usize)> {
+        // keeping track of the parents which I can use to rebuild the path --- but have not
+        // proven to myself that it is the shortest path yet.
+
+        let mut frontier = VecDeque::new();
+        frontier.insert(0, from);
+
+        let mut came_from: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+        came_from.entry(from).or_insert((999, 999));
+
+        while !frontier.is_empty() {
+            let current = frontier.pop_front().unwrap();
+            for next in self.cell_neighbours.get(&current).unwrap().to_vec() {
+                if !came_from.contains_key(&next) {
+                    frontier.push_back(next);
+                    came_from.entry(next).or_insert(current);
+                }
+            }
+        }
+        return came_from;
     }
 
     fn display(&self) {
@@ -121,7 +145,15 @@ fn test_scan_actors_in_read_order() {
     board.display();
 
     // currenty just confirms there is a connection...
-    board.calculate_shortest_path((2,2), (2,5));
+    let mut buffer = Vec::new();
+    let to = (1, 5);
+    let results: HashMap<(usize, usize), (usize, usize)> = board.calculate_shortest_path2((4,1), to);
+    let mut current = &to;
+    while !(current == &(999, 999) || !results.contains_key(&current)) {
+        buffer.push(current.clone());
+        current = &results[&current];
+    }
+    println!("Shortest Path: {:?}", buffer);
 }
 
 
